f7fa2bacbb7f426057486d84262462054079947a
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2022-07-27 15:20:45 +0200

    Add a few keymaps

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -4,7 +4,7 @@ local keymap = vim.keymap.set
 local opts = { silent = true }
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+-- keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -16,3 +16,13 @@ keymap('', '<right>', '<nop>', opts)
 
 -- File explore
 keymap('n', '<leader>pv', ':Ex<CR>', opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Clear highlights
+keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
+
+-- Close buffers
+keymap("n", "<S-q>", "<cmd>bdelete!<CR>", opts)
