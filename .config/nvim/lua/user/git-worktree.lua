1b7ee696c5bea9c0515e117ea7fce480f06e1231
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2022-07-27 15:20:07 +0200

    Small git worktree changes

--- .config/nvim/lua/user/git-worktree.lua
+++ .config/nvim/lua/user/git-worktree.lua
@@ -1,14 +1,12 @@
-local status_ok, telescope = pcall(require, "git-worktree")
+local status_ok, gitworktree = pcall(require, "git-worktree")
 if not status_ok then
   return
 end
 
-require("git-worktree").setup({
+gitworktree.setup({
 --    change_directory_command = <str> -- default: "cd",
 --    update_on_change = <boolean> -- default: true,
 --    update_on_change_command = <str> -- default: "e .",
 --    clearjumps_on_change = <boolean> -- default: true,
 --    autopush = <boolean> -- default: false,
 })
-
-require("telescope").load_extension("git_worktree")
