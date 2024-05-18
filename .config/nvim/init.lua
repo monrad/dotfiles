4e025374697ee83f4facc62b755cef0ce0825c5d
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2024-05-18 14:54:05 +0200

    Add keymap for deleting buffers from telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -367,6 +367,19 @@ require("lazy").setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
+        defaults = {
+          -- Default configuration for telescope goes here:
+          -- config_key = value,
+          mappings = {
+            n = {
+              ["<c-d>"] = require("telescope.actions").delete_buffer,
+            }, -- n
+            i = {
+              ["<C-h>"] = "which_key",
+              ["<c-d>"] = require("telescope.actions").delete_buffer,
+            }, -- i
+          }, -- mappings
+        }, -- defaults
         -- pickers = {}
         extensions = {
           ["ui-select"] = {
