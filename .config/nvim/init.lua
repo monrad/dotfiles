2689678ad174500e6ea6abbad457c4df2d9a9446
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2024-06-07 12:25:53 +0200

    Lazy updates and fix lazydev dep

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -440,8 +440,15 @@ require("lazy").setup({
       {
         "folke/lazydev.nvim",
         ft = "lua",
-        opts = {},
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "luvit-meta/library", words = { "vim%.uv" } },
+          },
+        },
       },
+      -- optional `vim.uv` typings
       { "Bilal2453/luvit-meta", lazy = true },
     },
     config = function()
