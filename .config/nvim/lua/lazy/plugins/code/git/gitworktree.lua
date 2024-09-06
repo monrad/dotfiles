f0fc2d1f25548e5232e1d7f83ea4b1a573fb6013
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2024-09-06 20:16:59 +0200

    Change over to polarmutex git worktree module

--- .config/nvim/lua/lazy/plugins/code/git/gitworktree.lua
+++ .config/nvim/lua/lazy/plugins/code/git/gitworktree.lua
@@ -1,6 +1,6 @@
 return {
-  "ThePrimeagen/git-worktree.nvim",
-  version = "*",
+  "polarmutex/git-worktree.nvim",
+  -- version = "^2",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
@@ -18,4 +18,20 @@ return {
     ":Telescope git_worktree create_git_worktree<CR>",
     { desc = "[g]it [w]orktree [c]reate" }
   ),
+  config = function()
+    local Hooks = require "git-worktree.hooks"
+    local config = require "git-worktree.config"
+    local update_on_switch = Hooks.builtins.update_current_buffer_on_switch
+
+    Hooks.register(Hooks.type.SWITCH, function(path, prev_path)
+      vim.notify("Switched from " .. prev_path .. " to " .. path)
+      update_on_switch(path, prev_path)
+      require("arrow.git").refresh_git_branch()
+      require("arrow.persist").load_cache_file()
+    end)
+
+    Hooks.register(Hooks.type.DELETE, function()
+      vim.cmd(config.update_on_change_command)
+    end)
+  end,
 }
