eaada16b746513ba792385614a9dded766c1f312
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2024-10-20 02:14:17 +0200

    Remove duplicated nvim_lsp cmp source

--- .config/nvim/lua/lazy/plugins/code/autocompletion.lua
+++ .config/nvim/lua/lazy/plugins/code/autocompletion.lua
@@ -132,7 +132,6 @@ return {
 					group_index = 0,
 				},
 				{ name = "nvim_lsp", max_item_count = 10 },
-				{ name = "nvim_lsp", max_item_count = 10 },
 				{ name = "luasnip", max_item_count = 10 },
 				{ name = "path", max_item_count = 5 },
 				{ name = "buffer", max_item_count = 5 },
