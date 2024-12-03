e01d855237f98ab376cd0b233cb0803d6fccb4ba
Mikkel Mondrup Kristensen <monrad@github.com>
2024-12-03 15:49:02 +0100

    Conform settings changed, from lazyvim

--- .config/nvim/lua/lazy/plugins/code/conform.lua
+++ .config/nvim/lua/lazy/plugins/code/conform.lua
@@ -73,6 +73,25 @@ return {
 			desc = "Toggle autoformat-on-save",
 			bang = true,
 		}),
+		formatters = {
+			["markdown-toc"] = {
+				condition = function(_, ctx)
+					for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+						if line:find("<!%-%- toc %-%->") then
+							return true
+						end
+					end
+				end,
+			},
+			["markdownlint-cli2"] = {
+				condition = function(_, ctx)
+					local diag = vim.tbl_filter(function(d)
+						return d.source == "markdownlint"
+					end, vim.diagnostic.get(ctx.buf))
+					return #diag > 0
+				end,
+			},
+		},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			markdown = { "prettierd", "markdownlint-cli2", "markdown-toc" },
