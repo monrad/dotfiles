8d0d245557675516eb1a5610d5a4e420a53a0444
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2025-01-19 20:59:00 +0100

    Use snacks picker for todo

--- .config/nvim/lua/lazy/plugins/ui/todo-comments.lua
+++ .config/nvim/lua/lazy/plugins/ui/todo-comments.lua
@@ -25,7 +25,19 @@ return {
 			"<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>",
 			desc = "Todo/Fix/Fixme (Trouble)",
 		},
-		{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-		{ "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+		{
+			"<leader>st",
+			function()
+				Snacks.picker.todo_comments()
+			end,
+			desc = "Todo",
+		},
+		{
+			"<leader>sT",
+			function()
+				Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+			end,
+			desc = "Todo/Fix/Fixme",
+		},
 	},
 }
