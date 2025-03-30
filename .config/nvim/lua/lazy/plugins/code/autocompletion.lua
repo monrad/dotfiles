28ff2f7a8aeaf3d05f9a1da0c217d6a69c4d6463
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2025-03-30 23:28:29 +0200

    Let us try out blink-cmp for completion

--- .config/nvim/lua/lazy/plugins/code/autocompletion.lua
+++ .config/nvim/lua/lazy/plugins/code/autocompletion.lua
@@ -1,15 +1,17 @@
 -- Autocompletion
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
+	"saghen/blink.cmp",
+	event = "VimEnter",
+	version = "1.*",
 	dependencies = {
-		-- Snippet Engine & its associated nvim-cmp source
+		-- Snippet Engine
 		{
 			"L3MON4D3/LuaSnip",
+			version = "2.*",
 			build = (function()
-				-- Build Step is needed for regex support in snippets
-				-- This step is not supported in many windows environments
-				-- Remove the below condition to re-enable on windows
+				-- Build Step is needed for regex support in snippets.
+				-- This step is not supported in many windows environments.
+				-- Remove the below condition to re-enable on windows.
 				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
 					return
 				end
@@ -19,143 +21,161 @@ return {
 				-- `friendly-snippets` contains a variety of premade snippets.
 				--    See the README about individual language/framework/plugin snippets:
 				--    https://github.com/rafamadriz/friendly-snippets
-				{
-					"rafamadriz/friendly-snippets",
-					config = function()
-						require("luasnip.loaders.from_vscode").lazy_load()
-					end,
-				},
+				-- {
+				--   'rafamadriz/friendly-snippets',
+				--   config = function()
+				--     require('luasnip.loaders.from_vscode').lazy_load()
+				--   end,
+				-- },
 			},
+			opts = {},
 		},
-		"saadparwaiz1/cmp_luasnip",
-
-		-- Adds other completion capabilities.
-		--  nvim-cmp does not ship with all sources by default. They are split
-		--  into multiple repos for maintenance purposes.
-		"chrisgrieser/cmp-nerdfont",
-		"f3fora/cmp-spell",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-emoji",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
+		{
+			"Kaiser-Yang/blink-cmp-git",
+			dependencies = { "nvim-lua/plenary.nvim" },
+		},
 		"onsails/lspkind.nvim",
-		{ "petertriho/cmp-git", opts = {} },
-		{ "roobert/tailwindcss-colorizer-cmp.nvim", opts = {} },
+		"ribru17/blink-cmp-spell",
+		"folke/lazydev.nvim",
 	},
-	config = function()
-		-- See `:help cmp`
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
-		local tailwindcss = require("tailwindcss-colorizer-cmp")
-		luasnip.config.setup({})
-
-		cmp.setup({
-			formatting = {
-				format = lspkind.cmp_format({
-					before = tailwindcss.formatter,
-					mode = "symbol_text",
-					maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-					-- can also be a function to dynamically calculate max width such as
-					-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-					menu = {
-						nvim_lsp = "[LSP]",
-						luasnip = "[LuaSnip]",
-						path = "[Path]",
-						buffer = "[Buffer]",
-						emoji = "[Emoji]",
-						nerdfont = "[NerdFont]",
-						git = "[Git]",
-						spell = "[Spell]",
-					},
-				}),
-			},
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			completion = { completeopt = "menu,menuone,noinsert" },
-
-			-- For an understanding of why these mappings were
-			-- chosen, you will need to read `:help ins-completion`
+	--- @module 'blink.cmp'
+	--- @type blink.cmp.Config
+	opts = {
+		keymap = {
+			-- 'default' (recommended) for mappings similar to built-in completions
+			--   <c-y> to accept ([y]es) the completion.
+			--    This will auto-import if your LSP supports it.
+			--    This will expand snippets if the LSP sent a snippet.
+			-- 'super-tab' for tab to accept
+			-- 'enter' for enter to accept
+			-- 'none' for no mappings
+			--
+			-- For an understanding of why the 'default' preset is recommended,
+			-- you will need to read `:help ins-completion`
 			--
 			-- No, but seriously. Please read `:help ins-completion`, it is really good!
-			mapping = cmp.mapping.preset.insert({
-				-- Select the [n]ext item
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				-- Select the [p]revious item
-				["<C-p>"] = cmp.mapping.select_prev_item(),
+			--
+			-- All presets have the following mappings:
+			-- <tab>/<s-tab>: move to right/left of your snippet expansion
+			-- <c-space>: Open menu or open docs if already open
+			-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+			-- <c-e>: Hide menu
+			-- <c-k>: Toggle signature help
+			--
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			preset = "default",
+			-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 
-				-- scroll the documentation window [b]ack / [f]orward
-				-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+			--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+		},
 
-				-- Accept ([y]es) the completion.
-				--  This will auto-import if your LSP supports it.
-				--  This will expand snippets if the LSP sent a snippet.
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		appearance = {
+			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+		},
 
-				-- If you prefer more traditional completion keymaps,
-				-- you can uncomment the following lines
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				-- ["<Tab>"] = cmp.mapping.select_next_item(),
-				-- ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-
-				-- Manually trigger a completion from nvim-cmp.
-				--  Generally you don't need this, because nvim-cmp will display
-				--  completions whenever it has completion options available.
-				["<C-a>"] = cmp.mapping.complete(),
-
-				-- Think of <c-l> as moving to the right of your snippet expansion.
-				--  So if you have a snippet that's like:
-				--  function $name($args)
-				--    $body
-				--  end
-				--
-				-- <c-l> will move you to the right of each of the expansion locations.
-				-- <c-h> is similar, except moving you backwards.
-				["<C-l>"] = cmp.mapping(function()
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					end
-				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function()
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					end
-				end, { "i", "s" }),
-
-				-- For more advanced luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-			}),
-			sources = {
-				{
-					name = "lazydev",
-					-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-					group_index = 0,
+		sources = {
+			default = { "spell", "git", "lsp", "path", "snippets", "lazydev" },
+			providers = {
+				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+				git = {
+					module = "blink-cmp-git",
+					name = "Git",
+					opts = {
+						-- options for the blink-cmp-git
+					},
 				},
-				{ name = "nvim_lsp", max_item_count = 10 },
-				{ name = "luasnip", max_item_count = 10 },
-				{ name = "path", max_item_count = 5 },
-				{ name = "buffer", max_item_count = 5 },
-				{ name = "emoji", max_item_count = 5 },
-				{ name = "nerdfont", max_item_count = 5 },
-				{ name = "git" },
-				{
-					name = "spell",
-					max_item_count = 5,
-					keyword_length = 3,
-					option = {
-						keep_all_entries = false,
+				spell = {
+					name = "Spell",
+					module = "blink-cmp-spell",
+					opts = {
+						-- EXAMPLE: Only enable source in `@spell` captures, and disable it
+						-- in `@nospell` captures.
 						enable_in_context = function()
-							return true
+							local curpos = vim.api.nvim_win_get_cursor(0)
+							local captures = vim.treesitter.get_captures_at_pos(0, curpos[1] - 1, curpos[2] - 1)
+							local in_spell_capture = false
+							for _, cap in ipairs(captures) do
+								if cap.capture == "spell" then
+									in_spell_capture = true
+								elseif cap.capture == "nospell" then
+									return false
+								end
+							end
+							return in_spell_capture
 						end,
 					},
 				},
 			},
-		})
-	end,
+		},
+
+		snippets = { preset = "luasnip" },
+
+		-- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+		-- which automatically downloads a prebuilt binary when enabled.
+		--
+		-- By default, we use the Lua implementation instead, but you may enable
+		-- the rust implementation via `'prefer_rust_with_warning'`
+		--
+		-- See :h blink-cmp-config-fuzzy for more information
+		fuzzy = {
+			implementation = "lua",
+			sorts = {
+				function(a, b)
+					local sort = require("blink.cmp.fuzzy.sort")
+					if a.source_id == "spell" and b.source_id == "spell" then
+						return sort.label(a, b)
+					end
+				end,
+				-- This is the normal default order, which we fall back to
+				"score",
+				"kind",
+				"label",
+			},
+		},
+
+		-- Shows a signature help window while you type arguments for a function
+		signature = { enabled = true },
+
+		completion = {
+			menu = {
+				draw = {
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								local icon = ctx.kind_icon
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+									if dev_icon then
+										icon = dev_icon
+									end
+								else
+									icon = require("lspkind").symbolic(ctx.kind, {
+										mode = "symbol",
+									})
+								end
+
+								return icon .. ctx.icon_gap
+							end,
+
+							-- Optionally, use the highlight groups from nvim-web-devicons
+							-- You can also add the same function for `kind.highlight` if you want to
+							-- keep the highlight groups in sync with the icons.
+							highlight = function(ctx)
+								local hl = ctx.kind_hl
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+									if dev_icon then
+										hl = dev_hl
+									end
+								end
+								return hl
+							end,
+						},
+					},
+				},
+			},
+		},
+	},
 }
