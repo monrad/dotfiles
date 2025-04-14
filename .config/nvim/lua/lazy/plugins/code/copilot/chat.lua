7c83fab0b93289afaf22057f3c4560b0b910fdd2
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2025-04-15 00:31:45 +0200

    Add more prompts

--- .config/nvim/lua/lazy/plugins/code/copilot/chat.lua
+++ .config/nvim/lua/lazy/plugins/code/copilot/chat.lua
@@ -1,3 +1,19 @@
+local prompts = {
+	-- Code related prompts
+	Explain = "Please explain how the following code works.",
+	Review = "Please review the following code and provide suggestions for improvement.",
+	Tests = "Please explain how the selected code works, then generate unit tests for it.",
+	Refactor = "Please refactor the following code to improve its clarity and readability.",
+	FixCode = "Please fix the following code to make it work as intended.",
+	FixError = "Please explain the error in the following text and provide a solution.",
+	BetterNamings = "Please provide better names for the following variables and functions.",
+	Documentation = "Please provide documentation for the following code.",
+	-- Text related prompts
+	Summarize = "Please summarize the following text.",
+	Spelling = "Please correct any grammar and spelling errors in the following text.",
+	Wording = "Please improve the grammar and wording of the following text.",
+	Concise = "Please rewrite the following text to make it more concise.",
+}
 return {
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
@@ -7,6 +23,7 @@ return {
 		},
 		build = "make tiktoken", -- Only on MacOS or Linux
 		opts = {
+			prompts = prompts,
 			model = "claude-3.7-sonnet",
 			highlight_headers = false,
 			separator = "---",
@@ -122,7 +139,7 @@ return {
 				desc = "CopilotChat - Quick chat",
 			},
 			-- Fix the issue with diagnostic
-			{ "<leader>af", "<cmd>CopilotChatFixError<cr>", desc = "CopilotChat - Fix Diagnostic" },
+			{ "<leader>af", "<cmd>CopilotChatFix<cr>", desc = "CopilotChat - Fix Diagnostic" },
 			-- Clear buffer and chat history
 			{ "<leader>al", "<cmd>CopilotChatReset<cr>", desc = "CopilotChat - Clear buffer and chat history" },
 			-- Toggle Copilot Chat Vsplit
