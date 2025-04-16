9fda4fefeb478e8f9b7ac63af36cc81b9412537d
Mikkel Mondrup Kristensen <monrad@github.com>
2025-04-16 13:54:41 +0200

    Do mo in the codespace install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 # install.sh for GitHub codespaces
-# Make sure .config dir exists
+echo "cloning zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+echo "cloning zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+echo "cloning you-should-use-this"
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+echo "cloning powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+echo "make sure $HOME/.config/ exists"
 mkdir -p "$HOME"/.config/
-# Symlink ripgrep config dir
+echo "symlink ripgrep config dir"
 ln -s --relative .config/ripgrep "$HOME"/.config/ripgrep
-# Symlink neovim config dir
+echo "symlink neovim config dir"
 ln -s --relative .config/nvim "$HOME"/.config/nvim
+echo "symlink git config dir"
+ln -s --relative .config/git "$HOME"/.config/git
+echo "symlink zshrc"
+ln -s --relative .zshrc "$HOME"/.zshrc
+echo "symlink p10k.zsh"
+ln -s --relative .p10k.zsh "$HOME"/.p10k.zsh
