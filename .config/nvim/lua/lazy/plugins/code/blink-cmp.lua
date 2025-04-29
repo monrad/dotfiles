5a63cbd50715f02a9dfdafffd72c425c98e5d03d
Mikkel Mondrup Kristensen <monrad@github.com>
2025-04-29 18:02:34 +0200

    Only enable git blink for octo, gitcommit and markdown

--- .config/nvim/lua/lazy/plugins/code/blink-cmp.lua
+++ .config/nvim/lua/lazy/plugins/code/blink-cmp.lua
@@ -83,6 +83,12 @@ return {
 				git = {
 					module = "blink-cmp-git",
 					name = "Git",
+					-- only enable this source when filetype is gitcommit, markdown, or 'octo'
+					enabled = function()
+						return vim.tbl_contains({ "octo", "gitcommit", "markdown" }, vim.bo.filetype)
+					end,
+					--- @module 'blink-cmp-git'
+					--- @type blink-cmp-git.Options
 					opts = {
 						-- options for the blink-cmp-git
 					},
