75ad77742abd7fef17bf29fff989a20e30046b74
Mikkel Mondrup Kristensen <monrad@github.com>
2025-07-15 21:27:55 +0200

    Set some mappings that don't clash

--- .config/nvim/lua/lazy/plugins/code/git/git-conflict.lua
+++ .config/nvim/lua/lazy/plugins/code/git/git-conflict.lua
@@ -1,5 +1,14 @@
 return {
 	"akinsho/git-conflict.nvim",
 	version = "*",
-	config = true,
+	config = function()
+		require("git-conflict").setup({
+			default_mappings = false,
+		})
+		vim.keymap.set("n", "<leader>goo", "<cmd>GitConflictChooseOurs<cr>")
+		vim.keymap.set("n", "<leader>got", "<cmd>GitConflictChooseTheirs<cr>")
+		vim.keymap.set("n", "<leader>go0", "<cmd>GitConflictChooseNone<cr>")
+		vim.keymap.set("n", "<leader>gon", "<cmd>GitConflictNextConflict<cr>")
+		vim.keymap.set("n", "<leader>gop", "<cmd>GitConflictPrevConflict<cr>")
+	end,
 }
