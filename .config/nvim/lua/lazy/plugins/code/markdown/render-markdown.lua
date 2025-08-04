bade976e61f287788992b6fabf3bce7c5b920b3c
Mikkel Mondrup Kristensen <monrad@github.com>
2025-08-04 15:11:22 +0200

    feat: add blink completions for render-markdown
    Also add module and type information

--- .config/nvim/lua/lazy/plugins/code/markdown/render-markdown.lua
+++ .config/nvim/lua/lazy/plugins/code/markdown/render-markdown.lua
@@ -1,7 +1,10 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
 	opts = {
 		file_types = { "markdown", "copilot-chat", "codecompanion" },
+		completions = { blink = { enabled = true } },
 	},
 	ft = { "markdown", "copilot-chat", "codecompanion" },
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
