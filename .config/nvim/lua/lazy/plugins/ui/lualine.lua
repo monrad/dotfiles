858660a0e1a224880d6aefc3bd5655a940dba0cd
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2025-10-13 02:43:11 +0200

    Add sidekick status to lualine

--- .config/nvim/lua/lazy/plugins/ui/lualine.lua
+++ .config/nvim/lua/lazy/plugins/ui/lualine.lua
@@ -1,10 +1,52 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "nvim-tree/nvim-web-devicons", "folke/sidekick.nvim" },
 	config = function()
+		local icons = {
+			copilot = {
+				enabled = "",
+				disabled = "",
+				warning = "",
+				nes = {
+					has = "󰭺",
+					process = "󱋊",
+				},
+			},
+		}
+		local SidekickStatus = {
+			function()
+				local inline_completions_is_enabled = vim.lsp.inline_completion.is_enabled()
+				local sidekick_status = require("sidekick.status").get()
+				local sidekick_has_nes = require("sidekick.nes").have()
+				local sidekick_process_nes = next(require("sidekick.nes")._requests)
+				local icon = not inline_completions_is_enabled and icons.copilot.disabled
+					or sidekick_status.kind == "Warning" and icons.copilot.warning
+					or icons.copilot.enabled
+				local icon_nes = sidekick_process_nes and icons.copilot.nes.process
+					or sidekick_has_nes and icons.copilot.nes.has
+					or ""
+				return icon .. icon_nes
+			end,
+			color = function()
+				local sidekick_status = require("sidekick.status").get()
+				if sidekick_status then
+					return sidekick_status.kind == "Error" and "DiagnosticError"
+						or sidekick_status.busy and "DiagnosticWarn"
+						or sidekick_status.kind == "Normal" and "Special"
+						or nil
+				end
+			end,
+			cond = function()
+				local sidekick_has_status = require("sidekick.status").get() ~= nil
+				-- local inline_completions_is_enabled = vim.lsp.inline_completion.is_enabled()
+				-- return sidekick_has_status or inline_completions_is_enabled
+				return sidekick_has_status
+			end,
+		}
 		require("lualine").setup({
 			sections = {
 				lualine_c = {
+					SidekickStatus,
 					{
 						"filename",
 						path = 1,
