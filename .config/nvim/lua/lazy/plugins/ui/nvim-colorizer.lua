8b05bedbf3bcca52cca62a079af5d44d610ea78f
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2026-03-01 02:58:07 +0100

    update colorizer config

--- .config/nvim/lua/lazy/plugins/ui/nvim-colorizer.lua
+++ .config/nvim/lua/lazy/plugins/ui/nvim-colorizer.lua
@@ -2,12 +2,14 @@ return {
 	"catgoose/nvim-colorizer.lua",
 	event = "BufReadPre",
 	opts = {
-		filetypes = {
-			"html",
-			"javascript",
-			"css",
-			templ = {
-				tailwind = true,
+		options = {
+			filetypes = {
+				"html",
+				"javascript",
+				"css",
+				templ = {
+					tailwind = true,
+				},
 			},
 		},
 	},
